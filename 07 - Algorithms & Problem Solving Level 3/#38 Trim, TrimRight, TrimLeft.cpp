170999f73d5ace35dde8f23310a3f97c946ef33f
Mohamed Osama <102436778+medosama@users.noreply.github.com>
2025-03-13 16:43:29 +0200

    Update #38 Trim, TrimRight, TrimLeft.cpp

--- 07 - Algorithms & Problem Solving Level 3/#38 Trim, TrimRight, TrimLeft.cpp
+++ 07 - Algorithms & Problem Solving Level 3/#38 Trim, TrimRight, TrimLeft.cpp
@@ -13,9 +13,8 @@ string TrimString(string text, enTrimTypes type = enTrimTypes::All)
 	switch (type)
 	{
 	case All:
-		while (text[text.length() - 1] == ' ')
-			text.erase(text.length() - 2, text.length() - 1);
-		while (text[0] == ' ') text.erase(0, 1);
+		text = TrimString(text, enTrimTypes::Right);
+		text = TrimString(text, enTrimTypes::Left);
 		break;
 	case Right:
 		while (text[text.length() - 1] == ' ')
